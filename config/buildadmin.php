b809bb2c0842ea27d50884e924da1ba5d54136bc
yanghongwei <403236160@qq.com>
2026-02-03 10:59:34 +0800

    refactor:使用新的接口地址以避免可能的本地 hosts 配置影响

--- config/buildadmin.php
+++ config/buildadmin.php
@@ -76,5 +76,5 @@ return [
     // 版本号
     'version'               => 'v2.3.5',
     // 中心接口地址（用于请求模块市场的数据等用途）
-    'api_url'               => 'https://buildadmin.com',
+    'api_url'               => 'https://api.buildadmin.com',
 ];
